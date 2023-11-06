5541d05ee0709e84b017581b7c8e95f24ed29085
MarkoBitencCode <marko.bitenc@codelivery.team>
2023-11-06 17:46:45 +0100

    testing deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) =>
-  res.json({ message: "Hello Codelivery on Express app for Azure" })
+  res.json({ message: "Hello Codelivery on Express app for Azure test 1" })
 );
 app.listen(process.env.PORT || 80);
