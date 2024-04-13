45dc5120cbb798651c89c346f8cb2ccee44a87dd
MarkoBitencCode <marko.bitenc@codelivery.team>
2024-04-13 21:58:30 +0200

    test for pipeline

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) =>
-    res.json({ message: "Hello Codelivery, testing Azure branch deploy test 👍" })
+    res.json({ message: "Azure branch deploy test pipeline 👍" })
 );
 app.listen(process.env.PORT || 80);
